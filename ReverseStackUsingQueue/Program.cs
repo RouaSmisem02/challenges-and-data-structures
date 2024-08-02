00c88f98324864b451d8a4ce4c49d624eb60b60d
Roua Smisem <rouasmisem02@gmail.com>
2024-08-02 23:16:48 +0300

    cc09#

---
+++ ReverseStackUsingQueue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReverseStackUsingQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackWithReverse stack = new StackWithReverse();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            Console.WriteLine(stack); // Stack: Top -> 5 -> 4 -> 3 -> 2 -> 1
+            stack.ReverseStack();
+            Console.WriteLine(stack); // Stack: Top -> 1 -> 2 -> 3 -> 4 -> 5
+
+            stack.Push(6);
+            stack.Push(7);
+            Console.WriteLine(stack); // Stack: Top -> 7 -> 6 -> 1 -> 2 -> 3 -> 4 -> 5
+            stack.ReverseStack();
+            Console.WriteLine(stack); // Stack: Top -> 5 -> 4 -> 3 -> 2 -> 1 -> 6 -> 7
+        }
+    }
+}
---
+++ TestProject/StackWithReverseTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+
+namespace ReverseStackUsingQueue
+{
+    public class StackWithReverseTests
+    {
+        [Fact]
+        public void TestReverseMultipleElements()
+        {
+            StackWithReverse stack = new StackWithReverse();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            stack.ReverseStack();
+
+            Assert.Equal(1, stack.Pop());
+            Assert.Equal(2, stack.Pop());
+            Assert.Equal(3, stack.Pop());
+            Assert.Equal(4, stack.Pop());
+            Assert.Equal(5, stack.Pop());
+        }
+
+        [Fact]
+        public void TestReverseSingleElement()
+        {
+            StackWithReverse stack = new StackWithReverse();
+            stack.Push(1);
+
+            stack.ReverseStack();
+
+            Assert.Equal(1, stack.Pop());
+        }
+
+        [Fact]
+        public void TestReverseEmptyStack()
+        {
+            StackWithReverse stack = new StackWithReverse();
+
+            stack.ReverseStack();
+
+            Assert.Empty(stack);
+        }
+    }
+}
