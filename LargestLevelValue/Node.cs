f27764249e6eb4786c7631f3ba788c6e2e37ce04
Roua Smisem <rouasmisem02@gmail.com>
2024-08-26 22:06:30 +0300

    cc16#

---
+++ LargestLevelValue/Node.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestLevelValue
+{
+    public class Node
+    {
+        public int Value;
+        public Node Left;
+        public Node Right;
+
+        public Node(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public Node Root;
+
+        public List<int> LargestValueEachLevel()
+        {
+            List<int> result = new List<int>();
+
+            if (Root == null) return result;
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(Root);
+
+            while (queue.Count > 0)
+            {
+                int levelSize = queue.Count;
+                int maxAtLevel = int.MinValue;
+
+                for (int i = 0; i < levelSize; i++)
+                {
+                    Node currentNode = queue.Dequeue();
+
+                    if (currentNode.Value > maxAtLevel)
+                    {
+                        maxAtLevel = currentNode.Value;
+                    }
+
+                    if (currentNode.Left != null)
+                    {
+                        queue.Enqueue(currentNode.Left);
+                    }
+
+                    if (currentNode.Right != null)
+                    {
+                        queue.Enqueue(currentNode.Right);
+                    }
+                }
+
+                result.Add(maxAtLevel);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestProject/LargestLevelValueTests.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace TestProject
+//{
+//    public class LargestLevelValueTests
+//    {
+//        [Fact]
+//        public void TestLargestValueEachLevel()
+//        {
+//            BinaryTree Btree = new BinaryTree();
+//            Btree.Root = new Node(5);
+//            Btree.Root.Left = new Node(13);
+//            Btree.Root.Right = new Node(7);
+//            Btree.Root.Left.Left = new Node(3);
+//            Btree.Root.Left.Right = new Node(7);
+//            Btree.Root.Right.Left = new Node(12);
+//            Btree.Root.Right.Right = new Node(20);
+//            Btree.Root.Left.Left.Left = new Node(1);
+//            Btree.Root.Left.Left.Right = new Node(4);
+//            Btree.Root.Right.Left.Right = new Node(11);
+
+//            List<int> expected = new List<int> { 5, 13, 20, 11 };
+//            List<int> result = Btree.LargestValueEachLevel();
+
+//            Assert.Equal(expected, result);
+//        }
+
+//        [Fact]
+//        public void TestEmptyTree()
+//        {
+//            BinaryTree Btree = new BinaryTree();
+//            List<int> expected = new List<int> { };
+//            List<int> result = Btree.LargestValueEachLevel();
+//            Assert.Equal(expected, result);
+//        }
+//    }
+//}
