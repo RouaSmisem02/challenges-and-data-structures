ca8a59729706daee86ec21a9c56bb20cb494f4c4
Roua Smisem <rouasmisem02@gmail.com>
2024-09-28 20:38:21 +0300

    cc18#

---
+++ RightViewPrint/BinaryTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightViewPrint
+{
+    public class BinaryTree
+    {
+        public Node Root;
+
+        public void PrintRightView()
+        {
+            if (Root == null) return;
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(Root);
+
+            while (queue.Count > 0)
+            {
+                int levelSize = queue.Count;
+
+                for (int i = 1; i <= levelSize; i++)
+                {
+                    Node current = queue.Dequeue();
+
+                    // If it's the rightmost node of this level
+                    if (i == levelSize)
+                        Console.Write(current.Data + " ");
+
+                    if (current.Left != null)
+                        queue.Enqueue(current.Left);
+                    if (current.Right != null)
+                        queue.Enqueue(current.Right);
+                }
+            }
+        }
+    }
+}
---
+++ RightViewPrint/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RightViewPrint
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTree Btree = new BinaryTree();
+            Btree.Root = new Node(2);
+            Btree.Root.Left = new Node(3);
+            Btree.Root.Right = new Node(5);
+            Btree.Root.Left.Left = new Node(4);
+            Btree.Root.Right.Right = new Node(6);
+            Btree.Root.Left.Left.Right = new Node(7);
+
+            Console.WriteLine("Right View of Binary Tree:");
+            Btree.PrintRightView();  // Expected Output: 2 5 6 7
+        }
+    }
+
+}
